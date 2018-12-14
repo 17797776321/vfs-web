421624427ece0b49fb782113112d993c16553802
张宸 <531336750@qq.com>
2018-12-14 17:30:04 +0800

    添加服务选项

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { log } from 'util';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +13,24 @@ export class HomeComponent implements OnInit {
   bannerUrl:string = '../../../assets/images/banner.png'
   titleText:string = '一站式服务  助力企业转型'
   desText:string = '十大行业解决方案  数百款营销活动 公众号'
-  constructor() { }
+  serviceItems:[] = [
+    new ServiceItems('../../../assets/images/service1.png','电商平台'),
+    new ServiceItems('../../../assets/images/service2.png','微信营销'),
+    new ServiceItems('../../../assets/images/service3.png','APP定制'),
+    new ServiceItems('../../../assets/images/service4.png','系统开发'),
+    new ServiceItems('../../../assets/images/service5.png','网站建设'),
+    new ServiceItems('../../../assets/images/service6.png','客户案例')
+  ]
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.serviceItems)
   }
+}
 
+export class ServiceItems {
+  constructor(
+    public imgUrl:string,
+    public text:string
+  ){}
 }
