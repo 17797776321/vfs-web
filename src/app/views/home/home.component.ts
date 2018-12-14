f8c7b5b296d46fb214c225c84b7bcd3f5a49ecc7
张宸 <531336750@qq.com>
2018-12-14 16:29:59 +0800

    配置添加animate.css

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -9,6 +9,9 @@ export class HomeComponent implements OnInit {
   items:number[] = [
     1,2,3,4,5,6,7,8,9
   ]
+  bannerUrl:string = '../../../assets/images/banner.png'
+  titleText:string = '一站式服务  助力企业转型'
+  desText:string = '十大行业解决方案  数百款营销活动 公众号'
   constructor() { }
 
   ngOnInit() {
