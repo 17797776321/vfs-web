d5831088e4c3012ac3a3053f3114e4b433987284
张宸 <531336750@qq.com>
2018-12-17 09:24:28 +0800

    去demo数组

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -7,9 +7,6 @@ import { log } from 'util';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  items:number[] = [
-    1,2,3,4,5,6,7,8,9
-  ]
   bannerUrl:string = '../../../assets/images/banner.png'
   titleText:string = '一站式服务  助力企业转型'
   desText:string = '十大行业解决方案  数百款营销活动 公众号'
