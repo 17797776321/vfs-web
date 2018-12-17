f25a1c20868476dbb66be941146cd7ec60069b41
张宸 <531336750@qq.com>
2018-12-17 11:19:38 +0800

    服务项目

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -11,12 +11,12 @@ export class HomeComponent implements OnInit {
   titleText:string = '一站式服务  助力企业转型'
   desText:string = '十大行业解决方案  数百款营销活动 公众号'
   serviceItems:any[] = [
-    new ServiceItems('../../../assets/images/service1.png','电商平台'),
-    new ServiceItems('../../../assets/images/service2.png','微信营销'),
-    new ServiceItems('../../../assets/images/service3.png','APP定制'),
-    new ServiceItems('../../../assets/images/service4.png','系统开发'),
-    new ServiceItems('../../../assets/images/service5.png','网站建设'),
-    new ServiceItems('../../../assets/images/service6.png','客户案例')
+    new ServiceItems('../../../assets/images/service1.png','电商平台',false),
+    new ServiceItems('../../../assets/images/service2.png','微信营销',true),
+    new ServiceItems('../../../assets/images/service3.png','APP定制',true),
+    new ServiceItems('../../../assets/images/service4.png','系统开发',false),
+    new ServiceItems('../../../assets/images/service5.png','网站建设',false),
+    new ServiceItems('../../../assets/images/service6.png','客户案例',true)
   ]
   constructor() {}
 
@@ -28,6 +28,7 @@ export class HomeComponent implements OnInit {
 export class ServiceItems {
   constructor(
     public imgUrl:string,
-    public text:string
+    public text:string,
+    public bg:boolean
   ){}
 }
