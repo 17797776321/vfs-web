e75f9a53f76694782d5ad4aa5ded6e4d9a885e64
张宸 <531336750@qq.com>
2018-12-18 18:09:50 +0800

    电子商务

--- src/app/views/onlineretailers/onlineretailers.component.ts
+++ src/app/views/onlineretailers/onlineretailers.component.ts
@@ -11,5 +11,22 @@ export class OnlineretailersComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  electronics:any[] = [
+    new Electronics('../../../assets/images/banner3.png','用户体验设计','微风尚强大的设计实力支撑，帮助客户跳出千篇一律的传统商城模板式网站，提升超强用户体验的感官设计，更加有利于产品及品牌展示，从而提升消费者的整个购物体验！'),
+    new Electronics('../../../assets/images/banner3.png','用户体验设计','微风尚强大的设计实力支撑，帮助客户跳出千篇一律的传统商城模板式网站，提升超强用户体验的感官设计，更加有利于产品及品牌展示，从而提升消费者的整个购物体验！'),
+    new Electronics('../../../assets/images/banner3.png','用户体验设计','微风尚强大的设计实力支撑，帮助客户跳出千篇一律的传统商城模板式网站，提升超强用户体验的感官设计，更加有利于产品及品牌展示，从而提升消费者的整个购物体验！'),
+    new Electronics('../../../assets/images/banner3.png','用户体验设计','微风尚强大的设计实力支撑，帮助客户跳出千篇一律的传统商城模板式网站，提升超强用户体验的感官设计，更加有利于产品及品牌展示，从而提升消费者的整个购物体验！'),
+    new Electronics('../../../assets/images/banner3.png','用户体验设计','微风尚强大的设计实力支撑，帮助客户跳出千篇一律的传统商城模板式网站，提升超强用户体验的感官设计，更加有利于产品及品牌展示，从而提升消费者的整个购物体验！'),
+    new Electronics('../../../assets/images/banner3.png','用户体验设计','微风尚强大的设计实力支撑，帮助客户跳出千篇一律的传统商城模板式网站，提升超强用户体验的感官设计，更加有利于产品及品牌展示，从而提升消费者的整个购物体验！'),
+    new Electronics('../../../assets/images/banner3.png','用户体验设计','微风尚强大的设计实力支撑，帮助客户跳出千篇一律的传统商城模板式网站，提升超强用户体验的感官设计，更加有利于产品及品牌展示，从而提升消费者的整个购物体验！'),
+    new Electronics('../../../assets/images/banner3.png','用户体验设计','微风尚强大的设计实力支撑，帮助客户跳出千篇一律的传统商城模板式网站，提升超强用户体验的感官设计，更加有利于产品及品牌展示，从而提升消费者的整个购物体验！'),
+    new Electronics('../../../assets/images/banner3.png','用户体验设计','微风尚强大的设计实力支撑，帮助客户跳出千篇一律的传统商城模板式网站，提升超强用户体验的感官设计，更加有利于产品及品牌展示，从而提升消费者的整个购物体验！')
+  ]
+}
+export class Electronics {
+  constructor(
+    public introImg:string,
+    public title:string,
+    public content:string
+  ){}
 }
