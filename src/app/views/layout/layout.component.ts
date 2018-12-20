a749ab17f695b78df859850b3f7e7bb639cf4ae0
张宸 <531336750@qq.com>
2018-12-20 11:19:24 +0800

    去掉转场动画

--- src/app/views/layout/layout.component.ts
+++ src/app/views/layout/layout.component.ts
@@ -1,20 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { slideInAnimation } from '../../animations';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.less'],
-  animations: [ slideInAnimation ]
+  styleUrls: ['./layout.component.less']
 })
 export class LayoutComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
-  getAnimationData(outlet: RouterOutlet) {
-    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
 }
